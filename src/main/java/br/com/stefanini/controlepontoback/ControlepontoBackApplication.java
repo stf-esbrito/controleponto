06369684fee4935a499f0dfb839c60efd46e2ffd
stf-esbrito <esbrito@stefanini.com>
2017-12-23 20:01:22 -0200

    First commit

---
+++ src/main/java/br/com/stefanini/controlepontoback/ControlepontoBackApplication.java
@@ -0,0 +1,12 @@
+package br.com.stefanini.controlepontoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlepontoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlepontoBackApplication.class, args);
+	}
+}
