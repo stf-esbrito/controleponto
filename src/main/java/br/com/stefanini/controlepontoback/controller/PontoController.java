5444fffa4a3e49784e73922f9700e983da3bcf3b
Eduardo Santos de Brito <esbrito@latam.stefanini.com>
2017-12-26 11:16:20 -0200

    Criar controller

---
+++ src/main/java/br/com/stefanini/controlepontoback/controller/PontoController.java
@@ -0,0 +1,32 @@
+package br.com.stefanini.controlepontoback.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.stefanini.controlepontoback.dto.PontoDTO;
+import br.com.stefanini.controlepontoback.model.Ponto;
+import br.com.stefanini.controlepontoback.service.PontoService;
+
+@RestController
+@RequestMapping
+public class PontoController {
+
+	@Autowired
+	private PontoService pontoService;
+	
+	@GetMapping
+	public List<PontoDTO> findAll(){
+		return pontoService.findAll();
+	}
+	
+	@PostMapping("/save")
+	public Ponto save(@RequestBody PontoDTO ponto){
+		return pontoService.save(ponto);
+	}
+}
