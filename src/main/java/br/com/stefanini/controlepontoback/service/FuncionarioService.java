37f27a902621edf80603c4f8a4e294536887a1c0
Eduardo Santos de Brito <esbrito@latam.stefanini.com>
2017-12-26 13:12:13 -0200

    Implementar Funcionario Service

--- src/main/java/br/com/stefanini/controlepontoback/service/FuncionarioService.java
+++ src/main/java/br/com/stefanini/controlepontoback/service/FuncionarioService.java
@@ -1,8 +1,12 @@
 package br.com.stefanini.controlepontoback.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.stefanini.controlepontoback.converter.FuncionarioConverter;
+import br.com.stefanini.controlepontoback.dto.FuncionarioDTO;
 import br.com.stefanini.controlepontoback.repository.FuncionarioRepository;
 
 @Service
@@ -10,6 +14,10 @@ public class FuncionarioService {
 
 	@Autowired
 	private FuncionarioRepository funcionarioRepository;
-
+	
+	
+	public List<FuncionarioDTO> findAll(){
+		return FuncionarioConverter.toDTO(funcionarioRepository.findAll());
+	}
 	
 }
