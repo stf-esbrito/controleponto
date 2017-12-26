d0a20541d913b9b3484d5bc13d9f6ea84efc445c
Eduardo Santos de Brito <esbrito@latam.stefanini.com>
2017-12-26 09:39:33 -0200

    Criar repository

---
+++ src/main/java/br/com/stefanini/controlepontoback/repository/FuncionarioRepository.java
@@ -0,0 +1,11 @@
+package br.com.stefanini.controlepontoback.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.stefanini.controlepontoback.model.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
+
+}
---
+++ src/main/java/br/com/stefanini/controlepontoback/repository/PontoRepository.java
@@ -0,0 +1,11 @@
+package br.com.stefanini.controlepontoback.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.stefanini.controlepontoback.model.Ponto;
+
+@Repository
+public interface PontoRepository extends JpaRepository<Ponto, Long>{
+
+}
